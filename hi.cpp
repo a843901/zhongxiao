8a4167b27a85fdc2a8787d7ea9e390dde8c37c2e
a843901 <647246327@qq.com>
2025-10-01 09:59:48 +0800

    Add files via upload

---
+++ hi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main(void)
+{
+int a,b;
+
+cout<< "请输入第一个整数:"<<\n;;
+cin>>a;
+cout<<"请输入第二个整数:"<<\n;
+cin>>b;
+cout<<"两个数的和："<<a+b<<\n;
+cout<<"两个数的差："<<a-b<<\n;
+cout<<"两个数的积："<<a*b<<\n;
+cout<<"两个数的商："<<a/b<<\n;
+
+return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
