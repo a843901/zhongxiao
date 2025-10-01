3101b63bd518c1935bac016c89a0aa1ba68750b9
a843901 <647246327@qq.com>
2025-10-01 09:56:10 +0800

    Add files via upload

---
+++ helloo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+  int a=1;
+  for (int day=9;day>=1;day--)
+  { a=a*2+2; 
+
+
+
+
+  }
+  cout<<"小猴第一天一共摘了"<<a<<"个桃子"<<endl;
+  return 0;
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
